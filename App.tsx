8b6b6393ebaa6729d864544f859a90cf01a33f16
Antonio Rosa Junior <juniorrosa123@gmail.com>
2026-05-02 21:14:10 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Login/>
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#053155',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import {Text
+
+} from "react-native";
+import { View } from "react-native/types_generated/index";
+
+import { styles } from "./styles";
+
+export default function Login() {
+    return (
+        <View>
+            <View>
+                <View>
+
+                </View>
+            </View>
+        </View>>
+    );
+    }
